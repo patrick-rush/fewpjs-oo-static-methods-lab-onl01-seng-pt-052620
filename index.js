289e3bf06e6880cfff568bc376f1a0863d80ba44
patrick-rush <patrick.a.rush@gmail.com>
2020-12-26 13:23:30 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 class Formatter {
-  //add static methods here
+
+  static capitalize(string) {
+    return string[0].toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^\-\'\ A-Za-z0-9]/g, '')
+  }
+
+  static titleize(string) {
+    const ignoreWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    return this.capitalize(string).split(' ').map(function(word) {
+      if (ignoreWords.includes(word)) {
+        return word;
+      } else {
+        return Formatter.capitalize(word);
+      }
+    }).join(' ');
+  }
+
 }
